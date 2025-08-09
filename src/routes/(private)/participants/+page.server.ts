063ffc0ea30444121e8fe67e3a83f8ab11c77864
bhuynhdev <baoh.inbox@gmail.com>
2025-08-09 17:39:49 -0400

    First route /participants - template & load func

---
+++ src/routes/(private)/participants/+page.server.ts
@@ -0,0 +1,129 @@
+import { participant } from '$lib/server/db/schema'
+import { type AttendanceStatus } from '$lib/server/db/types'
+import { and, eq, like, sql } from "drizzle-orm"
+import type { PageServerLoad } from './$types'
+import "@total-typescript/ts-reset/filter-boolean"
+import { attendanceStatuses, DEFAULT_ITEMS_PER_PAGINATION } from '$lib/constants'
+
+export const load: PageServerLoad = async ({ locals, url }) => {
+  const db = locals.db
+
+  /** Fetch participants based on query string input parameters:
+   *  - query: Search that name and/or email includes the search phrase
+   *  - status: Specific attendance status
+   *  - pageNumber: Pagination
+   */
+  const query = url.searchParams.get("q")
+  const pageNumber = Number(url.searchParams.get("page") || 1)
+  const attendanceStatus = url.searchParams.get("status") as AttendanceStatus
+  const itemsPerPage = Number(url.searchParams.get("perpage") || DEFAULT_ITEMS_PER_PAGINATION)
+
+  const searchCriteria = [
+    query && like(participant.nameEmail, `%${query}%`),
+    attendanceStatus && eq(participant.attendanceStatus, attendanceStatus)
+  ].filter(Boolean);
+  const whereStatement = searchCriteria.length ? and(...searchCriteria) : undefined
+
+  const totalCount = await db.$count(participant, whereStatement)
+
+  // Offset-based pagination: Use Deferred Join technique to optimize https://orm.drizzle.team/docs/guides/limit-offset-pagination
+  const sq = db
+    .select({ id: participant.id })
+    .from(participant)
+    .where(whereStatement)
+    .orderBy(participant.createdAt, participant.id)
+    .limit(itemsPerPage)
+    .offset((pageNumber - 1) * itemsPerPage)
+    .as('subquery')
+  const participants = await db.select().from(participant).innerJoin(sq, eq(participant.id, sq.id)).orderBy(participant.createdAt, participant.id)
+
+  /**
+   * Calculate some participant statistics, including:
+   *  - Participants count by attendance statuses
+   */
+  const rawStatusCountArray = await db
+    .select({ status: participant.attendanceStatus, count: sql<number>`COUNT(*)`.mapWith(Number) })
+    .from(participant)
+    .groupBy(participant.attendanceStatus)
+
+  // Convert to object format { [status]: [count] }
+  const participantCountByStatus = Object.fromEntries(attendanceStatuses.map((status) => [status, 0])) as Record<AttendanceStatus, number> // Init with 0s
+  rawStatusCountArray.forEach(({ status, count }) => (participantCountByStatus[status] += count))
+
+  return {
+    totalCount,
+    participants: participants.map(({ participant }) => ({
+      ...participant,
+      availableAttendanceActions: getNextAttendanceActions(participant.attendanceStatus)
+    })),
+    alltimeStats: {
+      participantCountByStatus
+    }
+  }
+};
+
+export type AttendanceAction = 'CheckIn' | 'ConfirmAttendance' | 'Unconfirm' | 'ToggleLateCheckIn'
+
+/**
+ * State machine describing possible transitions given an input attendance status
+ * And return the next status given a particular transition
+ *
+ * Example:
+ * ```
+ * const newStatus = ATTENDANCE_STATUS_STATE_MACHINE[currentStatus]['CheckIn']
+ * ```
+ */
+const ATTENDANCE_STATUS_STATE_MACHINE: Record<AttendanceStatus, Partial<Record<AttendanceAction, AttendanceStatus>>> = {
+  registered: {
+    get ConfirmAttendance() {
+      // TODO: Check waitlist
+      const isWaitlisted = false
+      return isWaitlisted ? 'waitlist' : 'confirmed'
+    }
+  },
+  declined: {
+    get ConfirmAttendance() {
+      // Treat declined same as registered when it comes to confirming attendance
+      return ATTENDANCE_STATUS_STATE_MACHINE.registered.ConfirmAttendance
+    }
+  },
+  confirmed: {
+    CheckIn: 'attended',
+    ToggleLateCheckIn: 'confirmeddelayedcheckin',
+    Unconfirm: 'declined'
+  },
+  confirmeddelayedcheckin: {
+    CheckIn: 'attended',
+    ToggleLateCheckIn: 'confirmed'
+  },
+  waitlist: {
+    CheckIn: 'waitlistattended'
+  },
+  attended: {},
+  waitlistattended: {}
+} as const
+
+/**
+ * Given the current attendance status and action, determine the next attendance status
+ */
+function determineNextAttendanceStatus(args: { currentStatus: AttendanceStatus; action: AttendanceAction }): AttendanceStatus | null {
+  const currentState = ATTENDANCE_STATUS_STATE_MACHINE[args.currentStatus]
+  const potentialTransitions = Object.keys(currentState)
+  if (potentialTransitions.length == 0) {
+    return null // No more transition available at this state
+  }
+  if (args.action in currentState) {
+    return currentState[args.action as keyof typeof currentState] ?? null
+  }
+  throw Error(`AttendanceAtion ${args.action} is not possible given state ${args.currentStatus}`)
+}
+
+/**
+ * Determine the next attendance action to display on the UI
+ * given the current attendance status
+ */
+function getNextAttendanceActions(currentStatus: AttendanceStatus): Array<AttendanceAction> {
+  return Object.getOwnPropertyNames(ATTENDANCE_STATUS_STATE_MACHINE[currentStatus]) as Array<
+    keyof (typeof ATTENDANCE_STATUS_STATE_MACHINE)[AttendanceStatus]
+  >
+}
---
+++ src/routes/(private)/participants/+page.svelte
@@ -0,0 +1,178 @@
+<script lang="ts">
+  import type { PageProps } from './$types'
+  import { page } from '$app/state'
+  import { goto } from '$app/navigation'
+  import { attendanceStatuses, DEFAULT_ITEMS_PER_PAGINATION } from '$lib/constants'
+
+  const { data }: PageProps = $props()
+  const { totalCount, participants, alltimeStats } = $derived(data)
+  const { confirmed, confirmeddelayedcheckin, attended, waitlist, waitlistattended } = $derived(alltimeStats.participantCountByStatus)
+
+  let selectedParticipantId = $state<number | null>(null)
+  const participant = $derived(participants.find((p) => p.id == selectedParticipantId))
+
+  const attendanceStatusQs = $derived(page.url.searchParams.get('status') ?? '')
+  const pageNumber = $derived(Number(page.url.searchParams.get('page') ?? 1))
+  const itemsPerPage = $derived(Number(page.url.searchParams.get('perpage') ?? DEFAULT_ITEMS_PER_PAGINATION))
+
+  const currentPageFirstItemIndex = $derived((pageNumber - 1) * itemsPerPage + 1)
+  const currentPageLastItemIndex = $derived(Math.min(currentPageFirstItemIndex + itemsPerPage - 1, totalCount))
+
+  function goTonextPage() {
+    const query = new URLSearchParams(page.url.searchParams.toString())
+    query.set('page', `${pageNumber + 1}`)
+    goto(`?${query.toString()}`)
+  }
+
+  function goTopreviousPage() {
+    const query = new URLSearchParams(page.url.searchParams.toString())
+    query.set('page', `${Math.max(1, pageNumber - 1)}`)
+    goto(`?${query.toString()}`)
+  }
+</script>
+
+<div class="drawer drawer-end m-auto flex flex-col items-center justify-center gap-6">
+  <!-- Set participant to null if drawer is checked off -->
+  <input
+    id="participant-info-drawer"
+    type="checkbox"
+    class="drawer-toggle"
+    aria-hidden={true}
+    checked={selectedParticipantId !== null}
+    onchange={(e) => {
+      if (!e.currentTarget.checked) {
+        selectedParticipantId = null
+      }
+    }}
+  />
+  <div class="drawer-content w-full">
+    <div class="mb-6 grid grid-cols-3 gap-3">
+      <div class="rounded-lg bg-sky-200 p-4">
+        <p class="text-xl font-bold">{confirmed + confirmeddelayedcheckin + attended + waitlistattended}</p>
+        <p class="text-gray-500">Confirmed Attendance</p>
+      </div>
+      <div class="rounded-lg bg-emerald-200 p-4">
+        <p class="text-xl font-bold">{attended + waitlistattended}</p>
+        <p class="text-gray-500">Checked in</p>
+        <p class="text-gray-500 italic">{waitlistattended} from waitlist</p>
+      </div>
+      <div class="rounded-lg bg-amber-200 p-4">
+        <p class="text-xl font-bold">{waitlist}</p>
+        <p class="text-gray-500">Waitlist</p>
+      </div>
+    </div>
+    <form
+      method="get"
+      class="grid grid-flow-col grid-cols-[repeat(auto-fit,minmax(100px,1fr))] items-end gap-2 rounded-lg bg-gray-100 p-4"
+      role="search"
+    >
+      <label class="fieldset py-0">
+        <span class="fieldset-legend pt-0 text-sm">Name &amp; Email</span>
+        <input
+          class="input pb-0"
+          aria-label="Search participant"
+          type="text"
+          name="q"
+          placeholder="Search"
+          value={page.url.searchParams.get('q')}
+        />
+      </label>
+      <label class="fieldset py-0">
+        <span class="fieldset-legend pt-0 text-sm">Attendance status</span>
+        <select class="select" name="status">
+          <option value="" selected={!attendanceStatusQs}> --All-- </option>
+          {#each attendanceStatuses as status}
+            <option value={status} selected={status === attendanceStatusQs}>
+              {status}
+            </option>
+          {/each}
+        </select>
+      </label>
+      <button type="submit" class="btn btn-primary my-1 max-w-42"> Search </button>
+    </form>
+    <section aria-labelledby="participant-list-heading">
+      <div class="my-5 flex items-center gap-16">
+        <h2 id="participant-list-heading">Participant list</h2>
+        <div class="flex items-center gap-2">
+          <button
+            type="button"
+            class="btn btn-sm btn-soft btn-primary"
+            aria-label="Previous page"
+            title="Previous page"
+            onclick={goTopreviousPage}
+            disabled={pageNumber === 1}
+          >
+            <!-- <IconTablerChevronLeft /> -->
+            Back
+          </button>
+          <button
+            type="button"
+            class="btn btn-sm btn-soft btn-primary"
+            aria-label="Next page"
+            title="Next page"
+            onclick={goTonextPage}
+            disabled={currentPageLastItemIndex >= totalCount}
+          >
+            <!-- <IconTablerChevronRight /> -->
+            Next
+          </button>
+          <p class="ml-2 text-sm text-gray-600 italic">
+            {currentPageFirstItemIndex} - {currentPageLastItemIndex} of {totalCount}
+          </p>
+        </div>
+      </div>
+
+      <table aria-labelledby="participant-list-heading" class="table table-auto">
+        <thead>
+          <tr class="font-bold">
+            <th>Name &amp; Email</th>
+            <th>Status</th>
+            <th class="sr-only hidden md:table-cell">Edit</th>
+          </tr>
+        </thead>
+        <tbody>
+          {#each participants as p}
+            <tr
+              onpointerup={(e) => {
+                if (e.pointerType === 'touch') {
+                  selectedParticipantId = p.id
+                }
+              }}
+            >
+              <td>
+                <p>
+                  {p.firstName}
+                  {p.lastName}
+                </p>
+                <p class="text-gray-500 italic">{p.email}</p>
+              </td>
+              <td>
+                {p.attendanceStatus}
+                <!-- <AttendanceStatusBadge attendanceStatus={p.attendanceStatus} /> -->
+              </td>
+              <td class="hidden sm:table-cell">
+                <button
+                  aria-label="Open Participant edit modal"
+                  class="btn btn-primary h-8 min-h-8 text-white"
+                  onclick={() => (selectedParticipantId = p.id)}
+                >
+                  Edit
+                </button>
+              </td>
+            </tr>
+          {/each}
+        </tbody>
+      </table>
+    </section>
+  </div>
+  <div role="dialog" class="drawer-side">
+    <label for="participant-info-drawer" class="drawer-overlay"></label>
+    <div class="bg-base-100 min-h-full w-full max-w-[500px] p-6">
+      {#if participant}
+        Participant
+      {:else}
+        <p>No participant selected</p>
+      {/if}
+    </div>
+  </div>
+</div>
