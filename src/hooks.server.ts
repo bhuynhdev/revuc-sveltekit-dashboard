25c30ac25f2fc33ab6eefef1e495febff99800c9
bhuynhdev <baoh.inbox@gmail.com>
2025-08-11 12:42:45 -0400

    Pass in schema to Drizzle to instantiate Drizzle .query

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -1,12 +1,13 @@
 import type { Handle } from '@sveltejs/kit';
 import { drizzle } from 'drizzle-orm/d1';
+import * as schema from "$lib/server/db/schema"
 
 export const handle: Handle = async ({ event, resolve }) => {
   if (!event.platform) {
     throw Error("Cloudflare is not here!")
   }
   if (!event.locals.db) {
-    event.locals.db = drizzle(event.platform.env.DB)
+    event.locals.db = drizzle(event.platform.env.DB, { schema: schema })
   }
 
 	const response = await resolve(event);
