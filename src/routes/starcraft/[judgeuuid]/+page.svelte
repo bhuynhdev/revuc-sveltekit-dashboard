78979b24ce08d287e37b75d12b8f6426bfd2aa08
bhuynhdev <baoh.inbox@gmail.com>
2025-09-25 01:18:39 -0400

    Starcraft: Clicking project name open submisison details drawer

--- src/routes/starcraft/[judgeuuid]/+page.svelte
+++ src/routes/starcraft/[judgeuuid]/+page.svelte
@@ -10,36 +10,67 @@
   const judge = $derived(getJudgeByUuid(params.judgeuuid))
   // Judging has started if evalation records have been created
   const judgingHasStarted = $derived(!!judge.current?.assignedSubmissions.some((s) => s.evaluation))
+
+  let chosenSubmission = $state<SubmissionWithEvaluation | null>(null)
+  let drawerState = {
+    get checked() {
+      return !!chosenSubmission
+    },
+    set checked(checked) {
+      if (!checked) chosenSubmission = null
+    }
+  }
 </script>
 
-{#if judge.current}
-  {@const j = judge.current}
+<div class="drawer">
+  <input id="submission-details-drawer" type="checkbox" class="drawer-toggle" bind:checked={drawerState.checked} />
+  <div class="drawer-content">
+    {#if judge.current}
+      {@const j = judge.current}
 
-  {#if j.group}
-    <p>Hello {j.name} - Group: {j.group.name}</p>
-    <p><span class="italic">{j.category.name}</span> - {j.assignedSubmissions?.length || 0} projects</p>
-  {:else}
-    <p>No group assigned yet</p>
-  {/if}
-  {#if !judgingHasStarted}
-    <p>Judging has not started</p>
-  {/if}
+      {#if j.group}
+        <p>Hello {j.name} - Group: {j.group.name}</p>
+        <p><span class="italic">{j.category.name}</span> - {j.assignedSubmissions?.length || 0} projects</p>
+      {:else}
+        <p>No group assigned yet</p>
+      {/if}
+      {#if !judgingHasStarted}
+        <p>Judging has not started</p>
+      {/if}
 
-  {#if j.assignedSubmissions}
-    <div class="flex p-2 gap-2 flex-wrap justify-center">
-      {#each j.assignedSubmissions as submission, i}
-        {@render submissionView(submission, i)}
-      {/each}
+      {#if j.assignedSubmissions}
+        <div class="flex p-2 gap-2 flex-wrap justify-center">
+          {#each j.assignedSubmissions as submission, i (submission.id)}
+            {@render submissionView(submission, i)}
+          {/each}
+        </div>
+      {/if}
+    {/if}
+  </div>
+  <div class="drawer-side">
+    <label for="submission-details-drawer" aria-label="close sidebar" class="drawer-overlay"></label>
+    <div class="min-h-full w-4/5 bg-base-200 p-2">
+      <label for="submission-details-drawer" aria-label="close sidebar" class="btn btn-error">Close</label>
+      {#if chosenSubmission}
+        <h3 class='font-semibold text-lg'>{chosenSubmission.project.name}</h3>
+        <h4>Summary</h4>
+        <p class='mb-2'>lorem ipsum</p>
+        {#if chosenSubmission.evaluation}
+          {@render scoringForm(chosenSubmission.evaluation)}
+        {/if}
+      {:else}
+        No project selected
+      {/if}
     </div>
-  {/if}
-{/if}
+  </div>
+</div>
 
 {#snippet submissionView(submission: SubmissionWithEvaluation, listIndex: number)}
   {@const project = submission.project}
   <div class="p-3 outline-1 w-full max-w-lg">
     <div class="flex justify-between w-full gap-3 mb-2">
       <div class="flex gap-1.5 overflow-hidden whitespace-nowrap">
-        <button class="truncate">
+        <button class="drawer-button truncate" onclick={() => (chosenSubmission = submission)}>
           <span>{listIndex + 1}.</span>
           <span class="link">{project.name}</span>
         </button>
@@ -66,11 +97,11 @@
     <input type="hidden" name="judgeId" value={evaluation.judgeId} />
     <input type="hidden" name="submissionId" value={evaluation.submissionId} />
     <input type="hidden" name="judgeUuid" value={params.judgeuuid} />
-    <div class='space-y-2'>
+    <div class="space-y-2">
       {#each [1, 2, 3] as const as idx}
         <fieldset class="flex items-center gap-2">
-          <legend class="w-40 font-medium sr-only">Score {idx}</legend>
-          <label class="w-40 font-medium" for="score{idx}-{evaluation.submissionId}">Score {idx}</label>
+          <legend class="font-medium sr-only">Score {idx}</legend>
+          <label class="w-2/5 font-medium" for="score{idx}-{evaluation.submissionId}">Score {idx}</label>
           <div class="rating">
             {#each [1, 2, 3, 4, 5] as star}
               <input
